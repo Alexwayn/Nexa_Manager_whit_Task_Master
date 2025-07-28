c3962d5922e37851ca32feb91035a6ccc4a57a6f
DESKTOP-F6A69FR\user <rinelox@gmail.com>
2025-07-28 17:51:42 +0200

    Fix forwardRef error by adding explicit React import to main.tsx

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -1,7 +1,7 @@
 // Error handler - must be imported first
 import '@utils/error-handler';
 
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initToolbar } from '@stagewise/toolbar';
 import './index.css';
